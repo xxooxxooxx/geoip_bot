e0fda9f9acba21601b6ecdd5a71295976d1aa89c
xxooxxooxx <panchunmei30@gmail.com>
2021-09-02 18:34:13 +0800

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -3,7 +3,7 @@
 source /etc/profile
 curl https://download.db-ip.com/free/dbip-country-lite-$(date +'%Y-%m').mmdb.gz -o dbip-country-lite.mmdb.gz
 curl https://download.db-ip.com/free/dbip-country-lite-$(date +'%Y-%m').csv.gz -o dbip-country-lite.csv.gz
-curl https://github.com/torproject/tor/raw/release-0.4.5/src/config/mmdb-convert.py -o mmdb-convert.py
+curl https://raw.githubusercontent.com/torproject/tor/release-0.4.5/src/config/mmdb-convert.py -o mmdb-convert.py
 gunzip dbip-country-lite*.gz
 mkdir -p xt_geoip
 python mmdb-convert.py dbip-country-lite.mmdb>/dev/null 2>&1
